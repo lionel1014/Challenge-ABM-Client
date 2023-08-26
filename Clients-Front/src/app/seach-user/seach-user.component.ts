d6d179c5b5e390a6a5c983bb87df0a8a8521d622
Lionel Diez Gómez <satur.1512@gmail.com>
2023-08-25 23:33:31 -0300

    using the correct method of "subscribe", next

--- Clients-Front/src/app/seach-user/seach-user.component.ts
+++ Clients-Front/src/app/seach-user/seach-user.component.ts
@@ -20,16 +20,19 @@ export class SeachUserComponent {
     if(term.length <= 3)
       return
 
-    this.userService.searchUser(term).subscribe( users => {
-      this.users = users
-      this.hasClientFound = true
-      this.errorMessage = '';
-    },error => {
-      console.log(error)
-      this.hasClientFound = false;
-      this.errorMessage = error.error
-      this.users = [];
+    this.userService.searchUser(term).subscribe({
+      next: users => { //si no hay error llama aqui
+        this.users = users;
+        this.hasClientFound = true;
+        this.errorMessage = '';
+      },
+      error: error => {
+        console.log(error);
+        this.hasClientFound = false;
+        this.errorMessage = error.error;
+        this.users = [];
+      },//cuando se completa, tanto error o no pasa por el complete. es como el try, cath 
+      complete: () => console.info('complete papapa')
     });
   }
-
 }
